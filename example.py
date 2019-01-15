15e6af68d0df85739a2467ed8a3df320361c7c99
arose13 <anthony_rose12@hotmail.com>
2019-01-15 11:20:22 -0500

    Comparing convex opt with coordinate descent

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from numpy.testing import assert_almost_equal
 from scipy import stats
 from plasso import PliableLasso
 from plasso.helpers import model, objective, derivative_wrt_beta_j, derivative_wrt_theta_j
-from plasso.PliableLasso import OPTIMISE_COORDINATE
+from plasso.PliableLasso import OPTIMISE_COORDINATE, OPTIMISE_CONVEX
 from sklearn.metrics import r2_score, mean_squared_error
 
 
@@ -63,16 +63,43 @@ if __name__ == '__main__':
         print(f'dJ/dT_{pi} = {d_wrt_tj}')
         break
 
-    # Optimisation Test
     y_gt = y.copy()
     y += 0.5 * stats.norm().rvs(n)  # Add noise from paper
 
-    print('\n=== Fitting Model ===')
-    plasso = PliableLasso(lam=0.001, fit_intercepts=False, max_iter=100)
+    # Optimisation Test (Convex Optimisation)
+    print('\n=== Fitting Model via Convex Optimisation ===')
+    plasso = PliableLasso(lam=2, fit_intercepts=False, max_iter=100)
+    plasso.fit(x, z, y, optimizer=OPTIMISE_CONVEX)
+    y_hat = plasso.predict(x, z)
+
+    print('\n== Outputs ==')
+
+    print('\nbeta_0')
+    print(plasso.beta_0)
+
+    print('\ntheta_0')
+    print(plasso.theta_0)
+
+    print('\nbeta')
+    print(np.round(plasso.beta, 2))
+
+    print('\ntheta')
+    print(np.round(plasso.theta, 2))
+
+    print('--- Best Possible ---')
+    print(f'R2 = {r2_score(y, y_gt):0.2%}, MSE = {mean_squared_error(y, y_gt):0.5f}')
+    print(f'J = {objective(beta_0, theta_0, beta, theta, x, z, y, alpha=plasso.alpha, lam=plasso.lam):0.5f}')
+
+    print('--- Obtained ---')
+    print(f'R2 = {r2_score(y, y_hat):0.2%}, MSE = {mean_squared_error(y, y_hat):0.5f}')
+    print(f'J = {plasso.cost(x, z, y):0.5f}')
+
+    # Optimisation Test (Coordinate Descent)
+    print('\n=== Fitting Model via Coordinate Descent ===')
     plasso.fit(x, z, y, optimizer=OPTIMISE_COORDINATE)
     y_hat = plasso.predict(x, z)
 
-    print('\n=== Outputs ===')
+    print('\n== Outputs ==')
 
     print('\nbeta_0')
     print(plasso.beta_0)
