0b780b7702cb23bab4bd9df0c377fabd09076764
arose13 <anthony_rose12@hotmail.com>
2019-01-17 14:25:25 -0500

    first version code

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from codecs import open
 from setuptools import setup, find_packages
 
 
-__version__ = '0.20181222.1438'
+__version__ = '0.20190116.1424'
 
 here = path.abspath(path.dirname(__file__))
 
